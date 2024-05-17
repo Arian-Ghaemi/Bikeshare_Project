d01eb4d9f2b0f01e9be46e84fc454a3eaadbe5d8
Arian-Ghaemi <166465168+Arian-Ghaemi@users.noreply.github.com>
2024-05-17 16:23:14 +0200

    Update Bikeshare_Project.py

--- Bikeshare_Project.py
+++ Bikeshare_Project.py
@@ -21,17 +21,16 @@ col1, col2 = st.columns(2)
 #holiday_input = st.selectbox('Public Holiday/Weekend', ['Yes', 'No'])
 #weekday_input = st.selectbox('Weekend', ['Yes', 'No'])
 workingday_input = col1.selectbox('Weekend/Public Holiday', ['Yes', 'No'])
-weathersit_input = col1.selectbox('Weathersituation', ['1', '2', '3', '4'])
-col1.markdown("""
+temp_input = col2.number_input('Temperature', min_value=-89, value=None, max_value=57, step=1)
+weathersit_input = col2.selectbox('Weathersituation', ['1', '2', '3', '4'])
+col2.markdown("""
 - <span style='font-size: 12px;'>**1**: Clear, Few clouds, Partly cloudy, Partly cloudy</span>
 - <span style='font-size: 12px;'>**2**: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist</span>
 - <span style='font-size: 12px;'>**3**: Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds</span>
 - <span style='font-size: 12px;'>**4**: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog</span>
 """, unsafe_allow_html=True)
 
-temp_input = col2.number_input('Temperature', min_value=-89, value=None, max_value=57, step=1)
-hum_input = col2.number_input('Humidity', min_value=1, value=None ,max_value=100, step=1)
-windspeed_input = col2.number_input('Windspeed', min_value=0, value=None, max_value=113, step=1)
+
 
 
 # Convert categorical inputs to numerical if necessary
@@ -46,15 +45,6 @@ if weathersit_input is not None:
     weathersit = weathersit_input
 else: weathersit = 2
 
-if hum_input is not None:
-    hum = hum_input/100
-else: hum = 50
-
-if windspeed_input is not None:  
-    windspeed = windspeed_input/67
-else: windspeed = 30
-
-
 file_path = "rf_model.pkl"
 
 with open(file_path, 'rb') as f:
@@ -62,15 +52,20 @@ with open(file_path, 'rb') as f:
 
 inputs = []
 
-def cnt_predict(date, workingday, weathersit, temp, hum, windspeed):
+def cnt_predict(date, workingday, weathersit, temp):
     
     if date is not None:
-        year = date.year
+        if yr > 0:
+            yr = yr
+        else: yr=1
         month = date.month
         day = date.day
     else: 
         date = datetime.date(2011, 7, 6)
-        year = date.year
+        yr = date.year - 2011
+        if yr > 0:
+            yr = yr
+        else: yr=1
         month = date.month
         day = date.day
 
@@ -94,36 +89,35 @@ def cnt_predict(date, workingday, weathersit, temp, hum, windspeed):
             else:
                 return 4 #'Winter'  # This part ensures dates in Jan-Feb are winter
     
-    season = get_season(year, month, day)
+    season = get_season(yr, month, day)
     inputs.append(season)
-    inputs.append(year)    
+    inputs.append(yr)    
     inputs.append(workingday)
     inputs.append(weathersit)
     inputs.append(temp)
-    inputs.append(hum)
-    inputs.append(windspeed)
     inputs.append(month)
     inputs.append(day)
 
     return inputs
 
-inputs = cnt_predict(date, workingday, weathersit, temp, hum, windspeed)
+inputs = cnt_predict(date, workingday, weathersit, temp)
 
-col_names = ["season", "yr", "workingday", "weathersit", "temp", "hum", "windspeed", "month", "day"]
-inputs2 = np.array(inputs).reshape(1,9)
+col_names = ["season", "yr", "workingday", "weathersit", "temp", "month", "day"]
+inputs2 = np.array(inputs).reshape(1,7)
 df = pd.DataFrame(inputs2, columns=col_names)
 
 prediction = rf.predict(df)
 prediction = np.round(prediction)
 
-text = "the amount of users of this day:"
-st.markdown(f"<div style='font-size: 50px; text-align: center;'>{text} {prediction}</div>", unsafe_allow_html=True)
+text = "The amount of expected users on this day is:"
 
+if prediction > 4000:
+    colr = "red"
+elif 3000 < prediction <= 4000:
+    colr = "orange"
+elif 2000 < prediction <= 3000:
+    colr = "yellow"
+else:
+    colr = "green"
 
-# Inject custom CSS styles targeting a specific text input field
-
-
-
-
-
-
+st.markdown(f"<div style='font-size: 50px; text-align: center;'>{text}<br><span style='color: {colr};'>{prediction}</span>", unsafe_allow_html=True)
